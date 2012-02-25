893fb1c7fceb397cc6275e0f66b78d89a4e31f71
Pieter van Ginkel <pvginkel@gmail.com>
2012-02-25 07:34:20 +0100

    * Implemented service callback code generator.

--- ProtoChannel.CodeGenerator/ServerCallbackServiceGenerator.cs
+++ ProtoChannel.CodeGenerator/ServerCallbackServiceGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ProtoChannel.CodeGenerator
@@ -24,7 +25,99 @@ namespace ProtoChannel.CodeGenerator
 
         public override void Generate()
         {
-            throw new NotImplementedException();
+            if (Program.Arguments.Namespace != null)
+            {
+                WriteLine("namespace {0}", Program.Arguments.Namespace);
+                WriteLine("{");
+                Indent();
+            }
+
+            WriteClass();
+
+            if (Program.Arguments.Namespace != null)
+            {
+                Unindent();
+                WriteLine("}");
+            }
+        }
+
+        private void WriteClass()
+        {
+            WriteLine("[global::System.CodeDom.Compiler.GeneratedCode(\"{0}\", \"{1}\")]", GetType().Assembly.GetName().Name, GetType().Assembly.GetName().Version);
+            WriteLine("{0} partial class {1} : global::ProtoChannel.ProtoCallbackChannel", Modifier, Program.Arguments.ServerCallbackServiceName);
+            WriteLine("{");
+            Indent();
+
+            WriteMethods();
+
+            Unindent();
+            WriteLine("}");
+        }
+
+        private void WriteMethods()
+        {
+            bool hadOne = false;
+
+            foreach (var method in Program.ResolvedArguments.ServerServiceType.GetMethods())
+            {
+                var attribute = GetProtoMethodAttribute(method);
+
+                if (attribute == null)
+                    continue;
+
+                if (hadOne)
+                    WriteLine();
+                else
+                    hadOne = true;
+
+                if (((dynamic)attribute).IsOneWay)
+                    WritePostMethod(method);
+                else
+                    WriteSendMethod(method);
+            }
+        }
+
+        private void WritePostMethod(MethodInfo method)
+        {
+            WriteLine("[global::System.Diagnostics.DebuggerStepThrough]");
+            WriteLine("public void {0}(global::{1} message)", method.Name, method.GetParameters()[0].ParameterType.FullName);
+            WriteLine("{");
+            Indent();
+            WriteLine("PostMessage(message);");
+            Unindent();
+            WriteLine("}");
+        }
+
+        private void WriteSendMethod(MethodInfo method)
+        {
+            WriteLine("[global::System.Diagnostics.DebuggerStepThrough]");
+            WriteLine("public global::{0} {1}(global::{2} message)", method.ReturnType.FullName, method.Name, method.GetParameters()[0].ParameterType.FullName);
+            WriteLine("{");
+            WriteLine("    return End{0}(Begin{0}(message, null, null));", method.Name);
+            WriteLine("}");
+            WriteLine();
+            WriteLine("[global::System.Diagnostics.DebuggerStepThrough]");
+            WriteLine("public global::System.IAsyncResult Begin{0}(global::{1} message, global::System.AsyncCallback callback, object asyncState)", method.Name, method.GetParameters()[0].ParameterType.FullName);
+            WriteLine("{");
+            WriteLine("    return BeginSendMessage(message, typeof(global::{0}), callback, asyncState);", method.ReturnType.FullName);
+            WriteLine("}");
+            WriteLine();
+            WriteLine("[global::System.Diagnostics.DebuggerStepThrough]");
+            WriteLine("public global::{0} End{1}(global::System.IAsyncResult asyncResult)", method.ReturnType.FullName, method.Name);
+            WriteLine("{");
+            WriteLine("    return (global::{0})EndSendMessage(asyncResult);", method.ReturnType.FullName);
+            WriteLine("}");
+        }
+
+        private object GetProtoMethodAttribute(MethodInfo method)
+        {
+            foreach (var attribute in method.GetCustomAttributes(true))
+            {
+                if (attribute.GetType().FullName == "ProtoChannel.ProtoMethodAttribute")
+                    return attribute;
+            }
+
+            return null;
         }
     }
 }
