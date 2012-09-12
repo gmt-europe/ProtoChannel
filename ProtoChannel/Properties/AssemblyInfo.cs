0dc31195822b306e7a4e8aa5f49b22c2c1f29ea7
Pieter van Ginkel <pvginkel@gmail.com>
2012-09-12 07:44:18 +0200

    * Created version 1.2.1.0.

--- ProtoChannel/Properties/AssemblyInfo.cs
+++ ProtoChannel/Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("83c97d7b-d670-4092-9e1a-939ff675eca2")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
 
 [assembly: InternalsVisibleTo("ProtoChannel.Test-2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100dbd7de4850e5648f557cef197cb84e80775d3afedfeb52014692ec7908801fd2a9aeb5340ea7a2a5cd2f829dc0423c43c8519fca1475b9add9fa5423da3f445ba09fa7a2f613e2556d6ae4b3ecbb6d92da7f460647108f2f847eaf68648acefbe1668846beddae6d66943133e0404b73007b6ce98ddc396e21bd0f3f08ec7fce")]
 [assembly: InternalsVisibleTo("ProtoChannel.Test-4, PublicKey=0024000004800000940000000602000000240000525341310004000001000100dbd7de4850e5648f557cef197cb84e80775d3afedfeb52014692ec7908801fd2a9aeb5340ea7a2a5cd2f829dc0423c43c8519fca1475b9add9fa5423da3f445ba09fa7a2f613e2556d6ae4b3ecbb6d92da7f460647108f2f847eaf68648acefbe1668846beddae6d66943133e0404b73007b6ce98ddc396e21bd0f3f08ec7fce")]
