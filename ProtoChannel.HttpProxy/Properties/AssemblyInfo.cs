49dea2d04f58626051c9d099592734f88c766680
Pieter van Ginkel <pvginkel@gmail.com>
2013-02-10 09:46:15 +0100

    Created version 1.11.0.0.

--- ProtoChannel.HttpProxy/Properties/AssemblyInfo.cs
+++ ProtoChannel.HttpProxy/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5ae280e4-30c8-4301-80b7-c8b365597e12")]
 
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
