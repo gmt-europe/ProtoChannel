aeb6fdb67f660b6e1be4ec7c8a6991288c65b97c
Pieter van Ginkel <pvginkel@gmail.com>
2013-02-18 14:11:31 +0100

    Created version 1.12.1.0.

--- ProtoChannel.Test/Properties/AssemblyInfo.cs
+++ ProtoChannel.Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("6deacb66-c520-440b-83ab-3108bd5d0bfc")]
 
-[assembly: AssemblyVersion("1.12.0.0")]
-[assembly: AssemblyFileVersion("1.12.0.0")]
+[assembly: AssemblyVersion("1.12.1.0")]
+[assembly: AssemblyFileVersion("1.12.1.0")]
